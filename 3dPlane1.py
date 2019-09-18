c34ab58ed848e62d488f1a5f24c5d89a04992871
likhita26 <54359054+likhita26@users.noreply.github.com>
2019-09-18 08:52:38 +0530

    Update 3dPlane1.py

--- 3dPlane1.py
+++ 3dPlane1.py
@@ -12,23 +12,16 @@ import shlex
 fig = plt.figure()
 point1  = np.array([1, 1, 0])
 normal = np.array([1,-6, 2])
-
 point2 = np.array([1, 2, 3])
-
 point3 = np.array([3,1,1])
-
-
 # a plane is a*x+b*y+c*z+d=0
 # [a,b,c] is the normal. Thus, we have to calculate
 # d and we're set
 d = -point1.dot(normal)
-
 # create x,y
 xx, yy = np.meshgrid(range(10), range(10))
-
 # calculate corresponding z
 z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
-
 # plot the surface
 plt3d = plt.figure().gca(projection='3d')
 plt3d.plot_surface(xx, yy, z, alpha=0.6)
@@ -37,20 +30,14 @@ A1 = np.array([1,1,0]).reshape((3,1))
 l1 = np.array([2,1,4]).reshape((3,1))
 A2 =  np.array([1,1,0]).reshape((3,1))
 l2 = np.array([0,1,3]).reshape((3,1))
-
 #defining point of intersection
 P = np.array([1,1,0])
-
 #generating points in line 
 l1_p = line_dir_pt(l1,A1)
 l2_p = line_dir_pt(l2,A2)
-
 #plotting line
 plt.plot(l1_p[0,:],l1_p[1,:],l1_p[2,:],label="Line L1")
 plt.plot(l2_p[0,:],l2_p[1,:],l2_p[2,:],label="Line L2")
-
-
-
 #and i would like to plot this point : 
 plt3d.scatter(point2[0] , point2[1] , point2[2],  color='green')
 plt3d.text(point2[0] * (1 + 0.1), point2[1] * (1 - 0.1),point2[2]*(1) , 'B')
